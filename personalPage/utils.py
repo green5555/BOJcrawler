d94be3441d0df9587a834952858427edd26ba86c
green5555 <pasdfq@naver.com>
2019-03-31 18:51:00 +0900

    hongik only ver.2

--- personalPage/utils.py
+++ personalPage/utils.py
@@ -34,12 +34,16 @@ class UserPageCrawler:
         html = requested.text
         mySoup = BeautifulSoup(html, 'html.parser')
 
-        school_selector = '#statics > tbody > tr:nth-child(21) > td > a'
-        print([i.get('href') for i in mySoup.select(school_selector)])
-        if '/school/ranklist/436' not in [i.get('href') for i in mySoup.select(school_selector)] :
-            raise Http404("홍익대학교 학생이 아니네요")
-        
-        problem_selector = 'body > div.wrapper > div.container.content > div.row > div:nth-child(2) > div:nth-child(3) > div.col-md-9 > div:nth-child(1) > div.panel-body > span'
+        school_selector = '#statics > tbody > tr'
+        is_hongik_flag = False
+        for item in mySoup.select(school_selector) :
+            if item.select_one('th').get_text() == '학교/회사' :
+                if item.select_one('td > a[href = "/school/ranklist/436"]') is not None :
+                    is_hongik_flag = True
+        if is_hongik_flag == False :
+            raise Http404('홍익대 학생만 이용할 수 있어요.')
+
+        problem_selector = 'body > div.wrapper > div.container.content > div.row > div:nth-of-type(2) > div:nth-of-type(3) > div.col-md-9 > div:nth-of-type(1) > div.panel-body > span'
         tag_list = mySoup.select(problem_selector)
         fetched_set = set()
         for i in range(0, len(tag_list), 2) :
